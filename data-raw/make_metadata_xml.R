e07e2d78c1b98396bfb9f779c8f45c284750d327
liz-fw <lstebbins@flowwest.com>
2023-08-01 10:37:17 -0700

    updates project funding element

--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -53,6 +53,7 @@ custom_units <- data.frame(id = c("number of rotations", "NTU", "revolutions per
 
 unitList <- EML::set_unitList(custom_units)
 
+edi_number <- "tisdale" # for now
 eml <- list(packageId = edi_number,
             system = "EDI",
             access = add_access(),
