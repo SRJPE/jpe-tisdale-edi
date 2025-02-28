03ccab87b1e7f9922b9c6d7d44ae0032ff87c700
InigoP <ipeng@flowwest.com>
2025-02-28 14:35:36 -0800

    add gitaction, clean zip folder

--- data-raw/clean_zip_data.R
+++ data-raw/clean_zip_data.R
@@ -9,8 +9,8 @@ fahrenheit_to_celsius <- function(F) {
   return((5/9) * (F - 32))
 }
 
-clean_catch <- function(raw_data_path){
-  readr::read_csv(raw_data_path) |> #updated query has visitTime2
+clean_catch <- function(clean_data_path){
+  readr::read_csv(clean_data_path) |> #updated query has visitTime2
     mutate(totalLength = as.numeric(totalLength)) |>
     arrange(subSiteName, visitTime) |>
     mutate(trap_start_date = ymd_hms(case_when(visitType %in% c("Continue trapping", "Unplanned restart", "End trapping") ~ lag(visitTime2),
@@ -20,8 +20,8 @@ clean_catch <- function(raw_data_path){
     filter(siteName != "Lower Feather River RST")
 }
 
-clean_trap <- function(raw_data_path){
-  readr::read_csv(raw_data_path) |>
+clean_trap <- function(clean_data_path){
+  readr::read_csv(clean_data_path) |>
     mutate(discharge = as.numeric(discharge),
            waterTemp = ifelse(waterTemp > 500, NA, waterTemp),
            counterAtStart = ifelse(counterAtStart == 33303251, NA, counterAtStart)) |>  # setting outlier of 551 in waterTemp to NA
@@ -36,8 +36,8 @@ clean_trap <- function(raw_data_path){
     filter(siteName != "Lower Feather River RST")
 }
 
-clean_recapture <- function(raw_data_path){
-  readr::read_csv(raw_data_path) |>
+clean_recapture <- function(clean_data_path){
+  readr::read_csv(clean_data_path) |>
     arrange(subSiteName, visitTime) |>
     mutate(trap_start_date = ymd_hms(case_when(visitType %in% c("Continue trapping", "Unplanned restart", "End trapping") ~ lag(visitTime2),
                                                T ~ visitTime)),
@@ -49,36 +49,32 @@ clean_recapture <- function(raw_data_path){
     filter(siteName != "Lower Feather River RST")
 }
 
-clean_release <- function(raw_data_path){
-  readr::read_csv(raw_data_path) |>
+clean_release <- function(clean_data_path){
+  readr::read_csv(clean_data_path) |>
     mutate(appliedMarkPosition = case_when(appliedMarkPosition == "Pelvic fin, right" ~ "Pelvic fin right",
                                            appliedMarkPosition == "Pelvic fin, left" ~ "Pelvic fin left",
                                           T ~ appliedMarkPosition))
 }
 
-clean_data <- function(raw_data_path, clean_data_path) {
+clean_data <- function(clean_data_path) {
   # Temporary directories for extraction and writing
-  folder_path <- "data/tisdale_test.zip"
+  folder_path <- "data/tisdale.zip"
   temp_dir <- tempdir()
   temp_dir <- normalizePath(temp_dir, winslash = "/")
   original_wd <- getwd()
 
   # Unzip new_path
   unzip(folder_path, exdir = temp_dir)
-  print(temp_dir)
-  new_file <- file.path(temp_dir, basename(raw_data_path))
-
-  # Load historic data
-  # historic_data <- readr::read_csv(historic_path)
+  new_file <- file.path(temp_dir, basename(clean_data_path))
   # Load and filter new data
-  clean_data <- if (grepl("tisdale_catch.csv", raw_data_path)) {
-    clean_catch(raw_data_path)
-  } else if(grepl("tisdale_recapture.csv", raw_data_path)) {
-    clean_recapture(raw_data_path)
-  }else if(grepl("tisdale_release.csv", raw_data_path)){
-    clean_release(raw_data_path)
+  clean_data <- if (grepl("tisdale_catch.csv", clean_data_path)) {
+    clean_catch(new_file)
+  } else if(grepl("tisdale_recapture.csv", clean_data_path)) {
+    clean_recapture(new_file)
+  }else if(grepl("tisdale_release.csv", clean_data_path)){
+    clean_release(new_file)
   }else{
-    clean_trap(raw_data_path)
+    clean_trap(new_file)
   }
   # Combine data
   # Write updated data back to the temporary directory
@@ -93,13 +89,34 @@ clean_data <- function(raw_data_path, clean_data_path) {
   setwd(original_wd)
 }
 
+clean_current_year <- function(clean_data_path){
+  clean_current_year_data <- if (grepl("current_year_tisdale_catch.csv", clean_data_path)) {
+    clean_catch(clean_data_path)
+  } else if(grepl("current_year_tisdale_recapture.csv", clean_data_path)) {
+    clean_recapture(clean_data_path)
+  }else if(grepl("current_year_tisdale_release.csv", clean_data_path)){
+    clean_release(clean_data_path)
+  }else{
+    clean_trap(clean_data_path)
+  }
+  # Combine data
+  # Write updated data back to the temporary directory
+  write_csv(clean_currenty_year, new_file)
+
+}
 # Paths for historical and new data
 path <- sort(c("tisdale_catch.csv",
                "tisdale_recapture.csv",
                "tisdale_release.csv",
                "tisdale_trap.csv"))
-raw_data_path <- paste0("data/", path)
-clean_data_path <- paste0("data/tisdale_test.zip/", path)
+# raw_data_path <- paste0("data/", path)
+clean_data_path <- paste0("data/tisdale.zip/", path)
 
 # Apply the function to all file pairs
-mapply(clean_data, raw_data_path, clean_data_path)
+mapply(clean_data, clean_data_path)
+
+current_year_path <- sort(c("data/current_year_tisdale_catch.csv",
+                            "data/current_year_tisdale_release.csv",
+                            "data/current_year_tisdale_recapture.csv",
+                            "data/current_year_tisdale_trap.csv"))
+mapply(clean_current_year_data, current_year_path)
